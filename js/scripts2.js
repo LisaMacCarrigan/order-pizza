60f5f243a9e269eb0605661a4e52141f7b968140
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-08-28 16:42:11 -0400

    begin adding JS to determin base price of pizza based on size selection, add JS to display Order Total

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,22 +1,41 @@
 // BUSINESS -BACK-END LOGIC
-var sizeBasePrices = [8, 10, 12, 14, 16];
-var toppingOptions = ["Bacon", "Broccoli", "Mushroom", "Onion", "Tomato", "Extra Cheese"];
-var toppings = []; //maybe push options to this array?
+// var sizeBasePrices = [8, 10, 12, 14, 16];
+// var toppingOptions = ["Bacon", "Broccoli", "Mushroom", "Onion", "Tomato", "Extra Cheese"];
+// var toppings = []; //maybe push options to this array?
 
 // A pizza object constructor with properties for toppings and size.
 function Pizza (toppings, size) {
   this.pizzaToppings = [];
   this.pizzaSize = size;
-  this.pizzaCost = 0; //either set to size, or initialize at 0
+  this.pizzaCost = 0; //initialize at 0
 }
 
-// A prototype method for the cost of a pizza depending on the selections chosen.
-// Pizza.prototype.toppingsCount
+// A prototype method for the cost of a pizza depending on the number of toppings chosen.
+Pizza.prototype.toppingsCost = function (toppingsAmount) {
+  this.pizzaCost = toppingsAmount * 0.50;
+};
 
+Pizza.prototype.basePrice = function(selectedSize) {
+  if (this.pizzaSize === "Small") {
+    this.pizzaCost += 8.00;
+  }
+  else if (this.PizzaSize === "Medium") {
+    this.pizzaCost += 10.00;
+  }
+  else if (this.PizzaSize === "Large") {
+    this.pizzaCost += 12.00;
+  }
+  else if (this.PizzaSize === "Extra-large") {
+    this.pizzaCost += 14.00;
+  }
+  else {
+    this.pizzaCost += 16.00;
+  }
+};
 
-// Pizza.prototype.basePrice = function(size) {
-//   if (size )
-// }
+Pizza.prototype.totalCost = function() {
+
+};
 
 
 
@@ -24,24 +43,35 @@ function Pizza (toppings, size) {
 
 
 $(document).ready(function() {
+    // Submit selected options
   $("#pizza-order").submit(function(event) {
     event.preventDefault();
-    $("#chosen-toppings-displayed").text("");
 
+    $("#chosen-toppings-displayed").text(""); //reset Selected Toppings
+    $("#calculated-cost").text(""); //reset Order Total
 
     // A customer can select one pizza size and see their selected pizza size displayed
     var inputtedSize = $("input:radio[name=size]:checked").val();
-    console.log(inputtedSize); // passed test
     $("#chosen-size-displayed").text(inputtedSize).val();
-
-    //take inputtedSize and pass into function that will determine basePrice
 
     // A customer can select pizza toppings and see their selected toppings displayed
     var inputtedToppings = $("input:checkbox[name=topping]:checked").val();
     console.log(inputtedToppings); // passed test
     $("#chosen-toppings-displayed").append(inputtedToppings).val();
 
+    // The number of toppings selected
+    var toppingsQuantity = $("input[name='topping']:checked").length;
+    console.log(toppingsQuantity);
 
+    // Create an array of checked Toppings
+    // $("input:checkbox[name=topping]:checked").each(function(){
+    //     toppingsArray.push($(this).val());
+    // });
+    var newPizza = new Pizza(inputtedToppings, inputtedSize);
+    //Take inputtedSize value and pass into function that will determine basePrice
+    newPizza.basePrice(); // REMEMBER TO PARSE FLOAT***
+    console.log(newPizza.pizzaCost);
+    $("#calculated-cost").text("$ " + newPizza.pizzaCost);
 
   }); //end submit
 
