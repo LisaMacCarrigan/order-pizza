d07d9ef9b6c040afa583fd728a8015d425f194b3
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-08-28 15:12:59 -0400

    change Size input to radio, modify checkbox selection

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,11 +1,13 @@
 // BUSINESS -BACK-END LOGIC
 var sizeBasePrices = [8, 10, 12, 14, 16];
+var toppingOptions = ["Bacon", "Broccoli", "Mushroom", "Onion", "Tomato", "Extra Cheese"];
+var toppings = []; //maybe push options to this array?
 
 // A pizza object constructor with properties for toppings and size.
 function Pizza (toppings, size) {
   this.pizzaToppings = [];
   this.pizzaSize = size;
-  this.pizzaCost = size; //either set to size, or initialize at 0
+  this.pizzaCost = 0; //either set to size, or initialize at 0
 }
 
 // A prototype method for the cost of a pizza depending on the selections chosen.
@@ -24,20 +26,20 @@ function Pizza (toppings, size) {
 $(document).ready(function() {
   $("#pizza-order").submit(function(event) {
     event.preventDefault();
+    $("#chosen-toppings-displayed").text("");
 
-    // var newPizza = new Pizza(inputtedToppings, inputtedSize);
-    // var inputtedToppings = [];
 
     // A customer can select one pizza size and see their selected pizza size displayed
-    var inputtedSize = $("option[value]").val();
-    console.log(inputtedSize);
-    var inputtedSizeDetail = $("#selected-size-input").val();
-    console.log(inputtedSizeDetail); // passed test
-    $("#chosen-size-displayed").text(inputtedSizeDetail).val();
-
+    var inputtedSize = $("input:radio[name=size]:checked").val();
+    console.log(inputtedSize); // passed test
+    $("#chosen-size-displayed").text(inputtedSize).val();
 
     //take inputtedSize and pass into function that will determine basePrice
 
+    // A customer can select pizza toppings and see their selected toppings displayed
+    var inputtedToppings = $("input:checkbox[name=topping]:checked").val();
+    console.log(inputtedToppings); // passed test
+    $("#chosen-toppings-displayed").append(inputtedToppings).val();
 
 
 
